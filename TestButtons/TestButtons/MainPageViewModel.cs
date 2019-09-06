08f9dd162366400662e84142b3e94c2186d974d2
Jesper Baltzersen <jbaltzersen@gnhearing.com>
2019-09-06 13:40:21 +0200

    fixing

---
+++ TestButtons/TestButtons/MainPageViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+using static TestButtons.BlobContainer;
+
+namespace TestButtons
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+
+        public MainPageViewModel()
+        {
+        }
+
+        private ICommand _buttonCommand;
+        public ICommand ButtonCommand
+        {
+            get
+            {
+                return _buttonCommand = (Command)(_buttonCommand ?? new Command(() => ChangeStuff()));
+            }
+            set
+            {
+                _buttonCommand = value;
+            }
+        }
+
+        private void ChangeStuff()
+        {
+            if (MaleSelected)
+            {
+                MaleSelected = false;
+                FemaleSelected = true;
+            }
+            else
+            {
+                MaleSelected = true;
+                FemaleSelected = false;
+            }
+        }
+
+        private bool _maleSelected;
+        public bool MaleSelected
+        {
+            get => _maleSelected;
+            set
+            {
+                SetProperty<bool>(ref _maleSelected, value);
+            }
+        }
+
+        private bool _femaleSelected;
+        public bool FemaleSelected
+        {
+            get => _femaleSelected;
+            set
+            {
+                SetProperty<bool>(ref _femaleSelected, value);
+            }
+        }
+    }
+}
